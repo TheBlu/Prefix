32976241ab448cdcf062218c4f51617587e6d7ba
tanel <tanel@localhost.localdomain>
2015-07-07 02:32:02 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Prefix
-Prefix/Polish notation math expression solver in D
+Prefix is a simple(4 basic operators) math expression solver written in D.
+It uses [Pegged](https://github.com/PhilippeSigaud/Pegged) for parsing the notation.
+
+There are no plans to add functionality, though requests are welcome.
---
+++ source/main.d
@@ -0,0 +1,96 @@
+import pegged.grammar;
+
+mixin(grammar(import("prefix.peg")));
+
+real solve(ParseTree tree)
+{
+	import std.conv : to;
+	import std.stdio : writeln;
+	// Convert to Prefix.Number
+	real result = 0;
+	switch (tree.name)
+	{
+		case "Prefix.Number":
+			
+			// Convert matched string to integer
+			result = to!real(tree.matches[0]);
+			break;
+		case "Prefix.Operand":
+			
+			// Solve Number or Operation
+			result = solve(tree.children[0]);
+			break;
+		case "Prefix.Operation":
+			
+			// Get operation type
+			auto opType = tree.children[0].children[0];
+			
+			// Get operand values
+			auto op1 = solve(tree.children[1]);
+			auto op2 = solve(tree.children[2]);
+			
+			// Do the appropriate operation
+			switch (opType.name)
+			{
+				case "Prefix.Add":
+					result = op1 + op2;
+					break;
+				case "Prefix.Subtract":
+					result = op1 - op2;
+					break;
+				case "Prefix.Multiply":
+					result = op1 * op2;
+					break;
+				case "Prefix.Divide":
+					result = op1 / op2;
+					break;
+				default:
+					break;
+			}
+			break;
+		case "Prefix":
+			result = solve(tree.children[0]);
+			break;
+		default:
+			break;
+	}
+	return result;
+}
+
+string helpText = import("help.txt");
+
+void main(string[] args)
+{
+	import std.stdio : writeln;
+	import std.array : join;
+	string target = "";
+	if (args.length == 1)
+	{
+		writeln(helpText);
+		return;
+	}
+	foreach (arg; args[1..$])
+	{
+		if (arg == "-h")
+		{
+			writeln(helpText);
+			return;
+		}
+	}
+	
+	target = args[1];
+	
+	auto tree = Prefix(target);
+	if (tree.successful)
+	{
+		writeln(solve(tree));
+	}
+	else
+	{
+		writeln("Error: Expression is in unrecognized format.", "\n",
+		        "Use spaces to separate operands.", "\n",
+		        "\n", helpText, "\n",
+		        "Parse log:", "\n",
+		        tree);
+	}
+}
